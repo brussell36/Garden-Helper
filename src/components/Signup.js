8a65ef1b9d3238627027d74b6f6c4df63c013ace
Ben Russell <benjaminrussell36@gmail.com>
2020-09-25 13:39:18 -0700

    Create SignUp function

---
+++ src/components/Signup.js
@@ -0,0 +1,48 @@
+import React from "react";
+import firebase from "firebase/app";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Header from "./Header";
+
+function SignUp() {
+  function doSignUp(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(function () {
+        console.log("Successfully signed up!");
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      });
+  }
+  return (
+    <React.Fragment>
+      <Header />
+      <Container className="container">
+        <h1>Sign Up</h1>
+        <Form onSubmit={doSignUp}>
+          <Form.Group>
+            <Form.Control type="text" name="email" placeholder="Email" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Sign up
+          </Button>
+        </Form>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default SignUp;
