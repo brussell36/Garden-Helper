7ac23a7f172cd6ceade2f901a393b7c37e96289a
Ben Russell <benjaminrussell36@gmail.com>
2020-09-25 13:58:44 -0700

    Add sign in component

---
+++ src/components/Signin.js
@@ -0,0 +1,46 @@
+import React from "react";
+import firebase from "firebase/app";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import { useHistory } from "react-router-dom";
+
+function SignIn() {
+  
+  let history = useHistory();
+
+  function doSignIn(event) {
+    event.preventDefault();
+    const email = event.target.signinEmail.value;
+    const password = event.target.signinPassword.value;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(function () {
+        console.log("Successfully signed in!");
+        history.push("/");
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      });
+  }
+
+  return(
+    <React.Fragment>
+      <Container>
+        <h1>Sign In</h1>
+        <Form onClick={doSignIn}>
+          <Form.Group>
+            <Form.Control type='text' name='signInEmail' placeholder='Email' />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control type='password' name='password' placeholder='Password' />
+          </Form.Group>
+          <Button type='submit' variant='primary'>Sign In</Button>
+        </Form>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default SignIn;
