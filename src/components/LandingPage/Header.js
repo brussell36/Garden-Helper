7eb0eb6f7666df32a66191c83eddec6eb35df8e4
Ben Russell <benjaminrussell36@gmail.com>
2020-10-02 11:21:26 -0700

    Refactor NavBar

--- src/components/LandingPage/Header.js
+++ src/components/LandingPage/Header.js
@@ -5,8 +5,8 @@ function Header() {
   return(
     <React.Fragment>
       <div className='title-section'>
-        <h1>Garden Helper</h1>
-        <h3>Find new and interesting plants to fit your garden.</h3>
+        <h1 className='title'>Garden Helper</h1>
+        <h3 className='under-title'>Find new and interesting plants to fit your garden.</h3>
       </div>
     </React.Fragment>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,21 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import { Link } from "react-router-dom";
 
 function NavBar(props) {
 
   return (
     <React.Fragment>
       <Navbar style={{ backgroundColor: "#7ea04d", boxShadow: "0 5px 15px rgba(0, 0, 0, .5)",}} sticky="top" expand="lg">
-        <Navbar.Brand as={Link} to="/">Garden Helper</Navbar.Brand>
+        <Navbar.Brand href="/">Garden Helper</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link as={Link} to='/signup'>Sign-up</Nav.Link>
-            <Nav.Link as={Link} to="/signin">Sign-in</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href='/signup'>Sign-up</Nav.Link>
+            <Nav.Link href="/signin">Sign-in</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href='/signout'>Sign-out</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
