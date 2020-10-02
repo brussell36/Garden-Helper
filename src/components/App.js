d993ff38e1e490572b433f34a89f85ec1440a9da
Ben Russell <benjaminrussell36@gmail.com>
2020-10-02 15:36:05 -0700

    Refactor App.js to connect to Firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withFirebase } from './Firebase';
 import './../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PlantControl from './PlantControl';
+import AuthUserContext from './Session';
 
-function App() {
+function App(props) {
+  const [authUser, setAuthUser] = useState(null);
+
+  props.firebase.auth.onAuthStateChanged(function(user) {
+    if(user) {
+      setAuthUser(user);
+    } else {
+      setAuthUser(null);
+    }
+  })
+
   return (
-    <React.Fragment>
+    <AuthUserContext.Provider value={authUser}>
       <PlantControl />
-    </React.Fragment>
+    </AuthUserContext.Provider>
   );
 }
 
-export default App;
+export default withFirebase(App);
