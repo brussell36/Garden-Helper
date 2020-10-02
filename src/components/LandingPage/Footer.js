ba7dd46d2f138613871c639b2b11e4aa047f6cf2
Ben Russell <benjaminrussell36@gmail.com>
2020-10-02 11:08:03 -0700

    Add footer to landing page

---
+++ src/components/LandingPage/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../css/Footer.css';
+
+function Footer() {
+  return(
+    <div className='footer'>
+      <p>© Copyright 2020 Garden Helper</p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Features from './Features';
+import Footer from './Footer';
 
 function LandingPage() {
   return(
     <React.Fragment>
       <Header />
       <Features />
+      <Footer />
     </React.Fragment>
   );
 }
