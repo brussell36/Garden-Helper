da69d08f1de738fc202e82dfc629d4263896f651
Ben Russell <benjaminrussell36@gmail.com>
2020-10-06 15:26:22 -0700

    Refactor LandingPage styles a little

--- src/components/LandingPage/About.js
+++ src/components/LandingPage/About.js
@@ -12,7 +12,7 @@ function About() {
         <h1 className='about-h1'>About This Site</h1>
         <Row>
           <Col className='col-md-6 about-col'>
-            <p>This site was designed to help people new to gardening figure out what kind of plants would be ideal for their climate and gardening ability. When the user selects the amount of sun their yard receives, how much water they get or how much they like to water, and what their soil is like, they will get back a list of plants that should match their criteria.</p>
+            <p className='about-text'>This site was designed to help people new to gardening figure out what kind of plants would be ideal for their climate and gardening ability. When the user selects the amount of sun their yard receives, how much water they get or how much they like to water, and what their soil is like, they will get back a list of plants that should match their criteria.</p>
           </Col>
           <Col className='col-md-6 about-col'>
             <Image className='about-img' src='https://images-na.ssl-images-amazon.com/images/I/918HS-mKyzL._AC_SL1500_.jpg' />
--- src/components/LandingPage/ImageCarousel.js
+++ src/components/LandingPage/ImageCarousel.js
@@ -8,16 +8,16 @@ function ImageCarousel() {
       <h1 className='carousel-title'>So Many Plants!</h1>
       <Carousel className='carousel-container' controls={false}>
         <Carousel.Item>
-          <Image className='carousel-img' alt='Beautiful Plants' src='https://www.gardeningknowhow.com/wp-content/uploads/2009/08/fuchsia-1.jpg' />
+          <Image className='carousel-img' src='https://www.gardeningknowhow.com/wp-content/uploads/2009/08/fuchsia-1.jpg' />
         </Carousel.Item>
         <Carousel.Item>
-          <Image className='carousel-img' alt='So Many Plants' src='https://www.gilmour.com/wp-content/uploads/2018/03/hydrangea-care.jpg' />
+          <Image className='carousel-img' src='https://www.gilmour.com/wp-content/uploads/2018/03/hydrangea-care.jpg' />
         </Carousel.Item>
         <Carousel.Item>
-          <Image className='carousel-img' alt='' src='https://gardenerspath.com/wp-content/uploads/2020/01/Purple-Hellebores-in-the-Garden.jpg' />
+          <Image className='carousel-img' src='https://gardenerspath.com/wp-content/uploads/2020/01/Purple-Hellebores-in-the-Garden.jpg' />
         </Carousel.Item>
         <Carousel.Item>
-          <Image className='carousel-img' src='https://okanaganxeriscape.org/static/images/plant/resized/P1190264_700x700.jpg' />
+          <Image className='carousel-img' src='https://img.sunset02.com/sites/default/files/styles/marquee_large_2x/public/image/2016/10/main/rosmarinus-officinalis-tuscan-blue.jpg' />
         </Carousel.Item>
         <Carousel.Item>
           <Image className='carousel-img' src='https://cdn.britannica.com/31/137431-050-6470C3A9/spirea-flower-Japanese-plant-shrub-much-Canada.jpg' />
