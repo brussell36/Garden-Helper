87ea65bb67803a5fb2b982e1b58e49e7ba459e0d
Ben Russell <benjaminrussell36@gmail.com>
2020-10-19 13:17:56 -0700

    Remove build from gitignore

---
+++ build/precache-manifest.a8992e14d06e73404fa76440c9a972ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02129ac23677a356ba691c40c5653357",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65cd1d71f8241be58fa1",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "62d13e9e97a14f17b4fb",
+    "url": "/static/css/main.1f01a924.chunk.css"
+  },
+  {
+    "revision": "65cd1d71f8241be58fa1",
+    "url": "/static/js/2.d4c925a1.chunk.js"
+  },
+  {
+    "revision": "62d13e9e97a14f17b4fb",
+    "url": "/static/js/main.a4140e89.chunk.js"
+  },
+  {
+    "revision": "4a4d764cf8afdfc598a7",
+    "url": "/static/js/runtime-main.8a9bd567.js"
+  },
+  {
+    "revision": "dea598085fd786beb5b94cba96a6886d",
+    "url": "/static/media/SecretGarden.dea59808.jpg"
+  }
+]);
